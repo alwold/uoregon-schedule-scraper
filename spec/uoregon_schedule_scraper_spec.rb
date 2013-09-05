5e9525dd52756dff8a681eaf30e8a9e0986eb03c
Al Wold <alwold@alwold.com>
2013-09-05 00:53:47 -0700

    convert ghetto test cases to rspec

--- spec/uoregon_schedule_scraper_spec.rb
+++ spec/uoregon_schedule_scraper_spec.rb
@@ -1,4 +1,29 @@
 require 'uoregon_schedule_scraper'
 
 describe UoregonScheduleScraper do
+  it "shows closed class as closed" do
+    s = UoregonScheduleScraper.new
+    s.get_class_status('201203', '31481').should eq(:closed)
+  end
+
+  it "loads class info" do
+    s = UoregonScheduleScraper.new
+    info = s.get_class_info('201203', '31481')
+  end
+
+  it "shows another closed class as closed" do
+    s = UoregonScheduleScraper.new
+    s.get_class_status('201203', '31481').should eq(:closed)
+# puts s.get_class_info('201230', 'stu150')
+  end
+
+  it "gets nil status for nonexistent class" do
+    s = UoregonScheduleScraper.new
+    s.get_class_status('201203', '13322').should be_nil
+  end
+
+  it "gets nil class info for nonexistent class" do
+    s = UoregonScheduleScraper.new
+    s.get_class_info('201203', '13322').should be_nil
+  end
 end
