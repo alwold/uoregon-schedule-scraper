d4f0feadb643899366b61f254df360962109fcc5
Al Wold <alwold@alwold.com>
2013-09-05 01:03:35 -0700

    more tests

--- spec/uoregon_schedule_scraper_spec.rb
+++ spec/uoregon_schedule_scraper_spec.rb
@@ -6,11 +6,23 @@ describe UoregonScheduleScraper do
     s.get_class_status('201203', '31481').should eq(:open)
   end
 
+  it "shows closed class as closed" do
+    s = UoregonScheduleScraper.new
+    s.get_class_status('201301', '17787').should eq(:closed)
+  end
+
   it "loads class info" do
     s = UoregonScheduleScraper.new
     info = s.get_class_info('201203', '31481')
     info.name.should eq("  ART 115   Surface, Space, & Time")
     info.schedule.should eq("t 1200-1350")
+  end
+
+  it "loads more class info" do
+    s = UoregonScheduleScraper.new
+    info = s.get_class_info('201301', '17787')
+    info.name.should eq("  AAA 408   Wrk Indv Brand Strateg")
+    info.schedule.should eq("  tba")
   end
 
   it "gets nil status for nonexistent class" do
